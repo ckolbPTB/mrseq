5387c12d44d3ec85b561e88bd61ea25d19b828ec
Patrick Schuenke <37338697+schuenke@users.noreply.github.com>
2025-02-17 12:41:37 +0100

    Add T2 multi-echo SE sequence and tests (#8)

---
+++ tests/scripts/test_t2_multi_echo_se_single_line.py
@@ -0,0 +1,41 @@
+"""Tests for Gold standard multi-echo SE sequence."""
+
+import numpy as np
+import pytest
+from sequences.scripts.t2_multi_echo_se_single_line import main as create_seq
+
+EXPECTED_DUR = 5120.000970  # defined 2025-02-06
+
+
+def test_default_seq_duration(system_defaults):
+    """Test if default values result in expected sequence duration."""
+    seq = create_seq(system=system_defaults, show_plots=False)
+    duration = seq.duration()[0]
+    assert duration == pytest.approx(EXPECTED_DUR)
+
+
+def test_seq_creation_error_on_short_te(system_defaults):
+    """Test if error is raised on too short echo time."""
+    with pytest.raises(ValueError):
+        create_seq(system=system_defaults, echo_times=np.array([1e-3, 2e-3]), show_plots=False)
+
+
+def test_seq_creation_error_on_short_tr(system_defaults):
+    """Test if error is raised on too short repetition time."""
+    with pytest.raises(ValueError):
+        create_seq(system=system_defaults, tr=5e-3, show_plots=False)
+
+
+def test_seq_duration_vary_params_without_changing_duration(system_defaults):
+    """Test if sequence duration is as expected."""
+    seq = create_seq(
+        system=system_defaults,
+        fov_xy=192e-3,  # default 128e-3
+        n_readout=192,  # default 128
+        slice_thickness=6e-3,  # default 8e-3
+        show_plots=False,
+        test_report=False,
+        timing_check=False,
+    )
+    duration = seq.duration()[0]
+    assert duration == pytest.approx(EXPECTED_DUR)
