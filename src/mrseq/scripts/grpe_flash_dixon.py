5d62bcfe0630d00878c8276130cf3ab23805acc7
Christoph Kolbitsch <christoph.kolbitsch@ptb.de>
2026-01-19 14:37:36 +0000

    adapt write seq

--- src/mrseq/scripts/grpe_flash_dixon.py
+++ src/mrseq/scripts/grpe_flash_dixon.py
@@ -11,6 +11,7 @@ from mrseq.utils import MultiEchoAcquisition
 from mrseq.utils import find_gx_flat_time_on_adc_raster
 from mrseq.utils import round_to_raster
 from mrseq.utils import sys_defaults
+from mrseq.utils import write_sequence
 from mrseq.utils.constants import GOLDEN_ANGLE_HALF_CIRCLE
 from mrseq.utils.ismrmrd import Fov
 from mrseq.utils.ismrmrd import Limits
@@ -454,6 +455,7 @@ def main(
     show_plots: bool = True,
     test_report: bool = True,
     timing_check: bool = True,
+    v141_compatibility: bool = True,
 ) -> pp.Sequence:
     """Generate a 3D FLASH sequence with radial phase encoding.
 
@@ -494,6 +496,8 @@ def main(
         Toggles advanced test report.
     timing_check
         Toggles timing check of the sequence.
+    v141_compatibility
+        Save the sequence in pulseq v1.4.1 for backwards compatibility.
     """
     if system is None:
         system = sys_defaults
@@ -586,7 +590,7 @@ def main(
 
     # save seq-file to disk
     print(f"\nSaving sequence file '{filename}.seq' into folder '{output_path}'.")
-    seq.write(str(output_path / filename), create_signature=True)
+    write_sequence(seq, str(output_path / filename), create_signature=True, v141_compatibility=v141_compatibility)
 
     if show_plots:
         seq.plot(time_range=(0, (n_dummy_spokes * n_rpe_points + 20) * (tr or min_tr)))
